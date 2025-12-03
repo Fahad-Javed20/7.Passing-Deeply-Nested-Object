a03c9d258ba34ff4454dce0b8677fd61f69c8e31
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-03 13:18:08 +0500

    make changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-
-import './App.css'
-import UserProfile from './components/UserProfile'
+import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  
-
   return (
     <>
       <UserProfile />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,7 +1,19 @@
 
 const UserProfile = () => {
   return (
-    <div>UserProfile</div>
+    <div className="max-h-screen w-2/5 shadow-2xl rounded-2xl bg-white">
+<h1 className="text-xl font-bold mt-1 text-left">Fahad Bin Javed</h1>
+<h1 className="text-lg font-bold mt-1 text-left">Contact Information:</h1>
+<p>Email</p>
+<p>phone</p>
+<h1 className="text-lg font-bold mt-1 text-left">Address Details:</h1>
+<p>Street, City, Country</p>
+<h1 className="text-lg font-bold mt-1 text-left">Eductaion Details:</h1>
+<p>highestLevel Eductaion</p>
+<p>Insitute Name, in 2019</p>
+<h1 className="text-lg font-bold mt-1 text-left">My Hobbies are:</h1>
+<p>Hobbies</p>
+    </div>
   )
 }
 
